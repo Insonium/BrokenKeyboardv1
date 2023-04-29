35e259f13c388a78253594c18abaa6c26e908b09
Insonium <ThisEmailDoesNotExist@server.fake>
2023-04-29 13:06:36 +0200

    ClearCommand finished

--- src/main/java/org/brokenkeyboard/commands/ClearCommandSlash.java
+++ src/main/java/org/brokenkeyboard/commands/ClearCommandSlash.java
@@ -1,4 +1,51 @@
 package org.brokenkeyboard.commands;
 
-public class ClearCommandSlash {
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class ClearCommandSlash extends ListenerAdapter {
+    public void onSlashCommandInteraction (SlashCommandInteractionEvent event){
+
+        if (event.getName().equals("clear")){
+            int anzahl = event.getOption("anzahl").getAsInt();
+            if (anzahl == 0){
+                event.reply("Du musst eine positive Zahl angeben!");
+            }
+
+            List<Message> messages = event.getChannel().getHistory().retrievePast(anzahl).complete();
+            //event.getChannel().purgeMessages(messages.toArray(new Message[0]))
+            event.getChannel().purgeMessages(messages).clear();
+
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.setTitle("Nachrichten wurden gelöscht!");
+
+            if (anzahl == 1){
+                builder.setDescription("Insgesamt wurde " + anzahl + " Nachricht gelöscht!");
+            }else {
+                builder.setDescription("Insgesamt wurden " + anzahl + " Nachrichten gelöscht!");
+            }
+
+
+
+            builder.setThumbnail("https://media.discordapp.net/attachments/871342424981663765/1042137951167057990/mull.png?width=567&height=675");
+            builder.setColor(new Color(86, 98, 248));
+
+
+
+
+            event.replyEmbeds(builder.build()).setEphemeral(true).queue(m -> m.deleteOriginal().queueAfter(5, TimeUnit.SECONDS));
+
+
+        }
+
+    }
 }
